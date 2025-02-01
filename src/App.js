0df781c125e9cb8847b1416a67c8056beae0bfde
Coleene <acevedocoleene@gmail.com>
2025-02-01 15:18:40 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This porject is created by{" "}
-        <a href="https://github.com/cmvce" target="blank_">
-          Coleene Acevedo
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/cmvce/weather-app" target="blank_">
-          open-sourced
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This porject is created by{" "}
+          <a href="https://github.com/cmvce" target="blank_">
+            Coleene Acevedo
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/cmvce/weather-app" target="blank_">
+            open-sourced
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
