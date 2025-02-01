9e756371879e915d188a806c297082e73d0d83de
Coleene <acevedocoleene@gmail.com>
2025-02-01 15:09:44 +0100

    added the Github repo Url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This porject is created by{" "}
+        <a href="https://github.com/cmvce" target="blank_">
+          Coleene Acevedo
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/cmvce/weather-app" target="blank_">
+          open-sourced
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
 
 export default App;
